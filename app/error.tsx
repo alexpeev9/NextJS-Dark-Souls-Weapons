752413aee94d1da9e3efd139f864bb84a59f364e
Aleksandar Peev <62713012+alexpeev9@users.noreply.github.com>
2023-12-02 00:37:33 +0200

    Update global error and loading

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,9 @@
 'use client'
 
 export default function Error({ error }: { error: Error }) {
-  return <h2 className='text-center'>An Error Occured!</h2>
+  return (
+    <h2 className='title-font animate-bounce text-center text-2xl md:text-4xl mt-10 md:mt-16'>
+      An Error Occured!
+    </h2>
+  )
 }
