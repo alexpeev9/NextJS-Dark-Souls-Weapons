9eed612750617aea2c67fd5ef13cad67027ef770
Aleksandar Peev <62713012+alexpeev9@users.noreply.github.com>
2023-12-14 22:51:51 +0200

    Add title of item to weapon and category page

--- app/weapons/[slug]/page.tsx
+++ app/weapons/[slug]/page.tsx
@@ -2,12 +2,15 @@ import { Metadata } from 'next'
 
 import WeaponDetail from '@/components/weapons/weapon-detail'
 
+import { convertSlug } from '@/utils/textHelper'
+
 export const metadata: Metadata = {
-  title: 'Weapons'
+  title: 'Weapon'
 }
 
 export default function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug
+  metadata.title = convertSlug(slug)
 
   return <WeaponDetail slug={slug} />
 }
